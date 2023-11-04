fdcf8443ae7a19287dbc3c0266067c017cad7149
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-04 17:42:35 +0800

    Created tables for notes and reflections, and added an extension method on ScoutReflectionItem to get associated badge list

---
+++ lib/logic/scout_badge_storage/tables/scout_notes_table.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: recursive_getters
+
+import 'package:drift/drift.dart';
+
+import '../database.dart';
+
+class ScoutNoteItems extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get scoutBadges => text().check(scoutBadges.isNotValue(""))();
+  TextColumn get title => text().check(title.isNotValue(""))();
+  TextColumn get content => text().check(content.isNotValue(""))();
+
+  DateTimeColumn get creationDate =>
+      dateTime().withDefault(currentDateAndTime)();
+  DateTimeColumn get lastEditDate =>
+      dateTime().withDefault(currentDateAndTime)();
+}
+
+extension ObtainAssociatedBadgeIDList on ScoutNoteItem {
+  List<int> get scoutBadgesList =>
+      scoutBadges.split(",").map((e) => int.parse(e)).toList();
+}
