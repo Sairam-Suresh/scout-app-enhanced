8b1e74f4381684a7d988a4b4d4da0afe7b9e326a
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-04 15:40:11 +0800

    Removed use of the database in main.dart (shifted to a riverpod provider), and made crashlytics only activate on non debug mode builds

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -12,43 +11,24 @@ import 'package:scout_app_enhanced/screens/main/experiences/experiences.dart';
 import 'package:scout_app_enhanced/screens/main/root.dart';
 import 'package:scout_app_enhanced/screens/main/settings/settings.dart';
 
-import 'logic/scout_badge_storage/database.dart';
 import 'screens/main/badge_catalogue/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Firebase Crashlytics
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
 
-  // Drift
-
-  // TODO: Move this to a riverpod provider as an abstraction
-  final database = AppDatabase();
-
-  // await database.into(database.scoutBadgeItems).insert(
-  //     ScoutBadgeItemsCompanion.insert(
-  //         badgeGiven: "",
-  //         certGiven: "",
-  //         completed: "",
-  //         description: "Hello world!",
-  //         imageUrl: "",
-  //         name: "",
-  //         parsedGoogleSheetInfo: false,
-  //         url: ""));
-
-  List<ScoutBadgeItem> allItems =
-      await database.select(database.scoutBadgeItems).get();
-
-  print('items in database: $allItems');
+  // Firebase Crashlytics (Should only run in debug mode)
+  if (!kDebugMode) {
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
 
   runApp(const ProviderScope(child: MyApp()));
 }
