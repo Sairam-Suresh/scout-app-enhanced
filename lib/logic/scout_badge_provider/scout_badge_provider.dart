60bdc9263d58268f0ec74e9527c5faac9e1a11a0
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-04 11:03:12 +0800

    Removed other scout_badge declaration to reduce conflicts

--- lib/logic/scout_badge_provider/scout_badge_provider.dart
+++ lib/logic/scout_badge_provider/scout_badge_provider.dart
@@ -1,22 +1,22 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:scout_app_enhanced/logic/scout_badge/scout_badge.dart';
-
-part 'scout_badge_provider.g.dart';
-
-@Riverpod()
-class ScoutBadgeNotifier extends _$ScoutBadgeNotifier {
-  @override
-  FutureOr<List<ScoutBadge>> build() async {
-    return [
-      ScoutBadge(
-          url: "",
-          name: "Abseiler",
-          description: "Amongus",
-          imageUrl: "",
-          parsedGoogleSheetInfo: false,
-          completed: "",
-          badgeGiven: "",
-          certGiven: "")
-    ];
-  }
-}
+// import 'package:riverpod_annotation/riverpod_annotation.dart';
+// import 'package:scout_app_enhanced/logic/scout_badge/scout_badge.dart';
+//
+// part 'scout_badge_provider.g.dart';
+//
+// @Riverpod()
+// class ScoutBadgeNotifier extends _$ScoutBadgeNotifier {
+//   @override
+//   FutureOr<List<ScoutBadge>> build() async {
+//     return [
+//       ScoutBadge(
+//           url: "",
+//           name: "Abseiler",
+//           description: "Amongus",
+//           imageUrl: "",
+//           parsedGoogleSheetInfo: false,
+//           completed: "",
+//           badgeGiven: "",
+//           certGiven: "")
+//     ];
+//   }
+// }
