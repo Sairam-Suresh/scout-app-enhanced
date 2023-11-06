33604300ea160d95dad9a4148b65ba0bc63fc0da
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-06 18:43:16 +0800

    Created a new Auth with Google Provider, to be implemented soon

---
+++ lib/logic/google_auth_provider/auth_provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'auth_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+class AuthNotifier extends _$AuthNotifier {
+  @override
+  String build() {
+    return "";
+  }
+}
