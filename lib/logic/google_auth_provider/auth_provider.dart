92e1a1772f4a40762de1ae4200901c556a556ecd
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-08 16:27:19 +0800

    Implemented Google Authentication Riverpod

--- lib/logic/google_auth_provider/auth_provider.dart
+++ lib/logic/google_auth_provider/auth_provider.dart
@@ -1,11 +1,28 @@
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:scout_app_enhanced/sensitive.dart';
 
 part 'auth_provider.g.dart';
 
 @Riverpod(keepAlive: true)
-class AuthNotifier extends _$AuthNotifier {
+class GoogleAuth extends _$GoogleAuth {
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: googleClientId,
+  );
+
   @override
-  String build() {
-    return "";
+  Future<GoogleSignInAccount?> build() async {
+    return await _googleSignIn.signInSilently();
+  }
+
+  Future<void> initiateSignIn() async {
+    state = AsyncValue.data(await _googleSignIn.signIn());
+    ref.invalidateSelf();
+  }
+
+  Future<void> signOut() async {
+    await _googleSignIn.signOut();
+    await _googleSignIn.disconnect();
+    ref.invalidateSelf();
   }
 }
